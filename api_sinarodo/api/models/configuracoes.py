1463a032c4ae157b58fff1ecef145e8e6153485f
Guilherme Severo <guilhermehmsevero@hotmail.com>
2018-12-03 23:19:16 -0200

    Alterações junto com o henrique

--- api_sinarodo/api/models/configuracoes.py
+++ api_sinarodo/api/models/configuracoes.py
@@ -6,5 +6,10 @@ class Configuracoes(models.Model):
         db_table = 'Configuracoes'
         ordering = ['id']
 
-    valor_por_ponto = models.FloatField(null=False, blank=False)
-    acrescimo_encarregado = models.FloatField(null=False, blank=False)
+    acrescimo_encarregado = models.FloatField(null=False, blank=True, default=10.0)
+    dias_em_campo = models.IntegerField(null=False, blank=True, default=15)
+    premio_seis = models.FloatField(null=False, blank=True, default=200.0)
+    premio_sete = models.FloatField(null=False, blank=True, default=400.0)
+    premio_oito = models.FloatField(null=False, blank=True, default=600.0)
+    premio_nove = models.FloatField(null=False, blank=True, default=800.0)
+    premio_dez = models.FloatField(null=False, blank=True, default=1000.0)
